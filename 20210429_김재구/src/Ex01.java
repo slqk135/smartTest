aaa4384360ff14b5d70fce576888fdddf74d279e
jaegu <jaegu@218.38.137.28>
2021-04-29 10:26:20 +0900

    202104291026김재구

---
+++ 20210429_김재구/src/Ex01.java
@@ -0,0 +1,11 @@
+
+public class Ex01 {
+	public void gugu()
+	{
+		for(int i = 2; i <=9; i++) {
+			for(int j =1; j<=9;  j++) {
+				System.out.println(i + " * " + j + " = " + i * j);
+			}
+		}
+	}
+}
---
+++ 20210429_김재구/src/Ex02.java
@@ -0,0 +1,11 @@
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		
+		Ex01 ex = new Ex01();
+		ex.gugu();
+		
+	}
+
+}
